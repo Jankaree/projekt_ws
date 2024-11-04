078f65265580fa1873a0507f45330e21a29af7e2
unknown <martin.jernstrom1@gmail.com>
2024-11-04 23:31:44 +0100

    CityCoordinate impletation

--- src/main/java/com/example/ws_projekt/Controller/WeatherController.java
+++ src/main/java/com/example/ws_projekt/Controller/WeatherController.java
@@ -2,10 +2,13 @@ package com.example.ws_projekt.Controller;
 
 
 import com.example.ws_projekt.Model.CityCoordinate;
+import com.example.ws_projekt.Model.WeatherModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -22,12 +25,38 @@ public class WeatherController {
     }
 
     private static final List<CityCoordinate> cities = List.of(
-            new CityCoordinate("Stockholm", 59.33,18.06),
-            new CityCoordinate("Seoul", 37.53,127.02),
-            new CityCoordinate("New-York", 40.73,-73.935),
-            new CityCoordinate("Tokyo", 35.65,139.839)
+            new CityCoordinate("Stockholm", 59.33, 18.06),
+            new CityCoordinate("Seoul", 37.53, 127.02),
+            new CityCoordinate("New-York", 40.73, -73.935),
+            new CityCoordinate("Tokyo", 35.65, 139.839)
     );
 
+    @GetMapping("/{city}")
+    public Mono<WeatherModel> getWeatherAtCity(@PathVariable String city) {
 
-}
+
+        for (CityCoordinate coordinate : cities) {
+            if (coordinate.getCity().equalsIgnoreCase(city)) {
+                return weatherWebClientConfig.get()
+                        .uri(uriBuilder -> uriBuilder
+                                .queryParam("latitude", coordinate.getLatitude())
+                                .queryParam("longitude", coordinate.getLongitude())
+                                .queryParam("hourly", "temperature_2m")
+                                .build())
+                        .retrieve()
+                        .bodyToMono(WeatherModel.class);
+
+            }
+        }
+
+        return Mono.error(new RuntimeException("City not found"));
+
+        }
+
+    }
+
+
+
+
+
 
