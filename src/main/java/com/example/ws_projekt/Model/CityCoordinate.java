3ed780f2168da0bd49f33a33170fee063aa7f540
unknown <martin.jernstrom1@gmail.com>
2024-11-06 23:13:50 +0100

    User and city entity modifacation + implementation in UserController

--- src/main/java/com/example/ws_projekt/Model/CityCoordinate.java
+++ src/main/java/com/example/ws_projekt/Model/CityCoordinate.java
@@ -1,15 +1,29 @@
 package com.example.ws_projekt.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city_coordinates")
 public class CityCoordinate {
 
-    private final String city;
-    private final double latitude;
-    private final double longitude;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private double latitude;
+    private double longitude;
+
+    public CityCoordinate() {}
 
     public CityCoordinate(String city, double latitude, double longitude) {
         this.city = city;
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getCity() {
