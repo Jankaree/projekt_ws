8591097f84edbe4921332a5b8754b4769901cac1
unknown <martin.jernstrom1@gmail.com>
2025-02-04 00:23:54 +0100

    loggedIntracking

--- src/main/java/com/example/ws_projekt/Repository/UserRepository.java
+++ src/main/java/com/example/ws_projekt/Repository/UserRepository.java
@@ -4,6 +4,9 @@ package com.example.ws_projekt.Repository;
 import com.example.ws_projekt.Model.UserModel.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
 
